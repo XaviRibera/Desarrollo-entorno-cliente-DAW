ac1c927bd24a7f4d084f6f8d09e4e545db20597a
XaviRibera <xaviribera2004@gmail.com>
2023-10-25 11:45:26 +0200

    25/10/2023

---
+++ UD_11/onchange/datos.js
@@ -0,0 +1,18 @@
+let datos =[
+        {
+            "trimestre":1,
+            "meses":["Enero","Febrero","Marzo"]
+        },
+        {
+            "trimestre":2,
+            "meses":["Abril","Mayo","Junio"]
+        },
+        {
+            "trimestre":3,
+            "meses":["Julio","Agosto","Septiembre"]
+        },
+        {
+            "trimestre":4,
+            "meses":["Octubre","Noviembre","Diciembre"]
+        }
+]
